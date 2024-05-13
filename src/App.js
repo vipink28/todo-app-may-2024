c2f273644c2788999924a4131d784c792b0288b1
vipink28 <vipinkalyan28@gmail.com>
2024-05-13 10:34:39 +0530

    Home Layout added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import TaskList from './pages/TaskList';
@@ -8,13 +8,21 @@ import CreateTask from './pages/CreateTask';
 import Profile from './pages/Profile';
 import PageNotFound from './pages/PageNotFound';
 import Navbar from './components/Navbar';
+import Login from './auth/Login';
+import Register from './auth/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='/' element={<Navigate to="/login" />} ></Route>
+
+        <Route path='/' element={<Home />}>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Route>
+
         <Route path='/about' element={<About />}></Route>
         <Route path='/task-list' element={<TaskList />}></Route>
         <Route path='/create-task' element={<CreateTask />}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import illustration from '../assets/illustration.png';
+import { Link, Outlet } from 'react-router-dom';
 
 function Home(props) {
     return (
         <div className='container-fluid h-100'>
             <div className="row h-100">
                 <div className='col-lg-6 h-100 d-flex align-items-center justify-content-center flex-column text-white bg-primary'>
-
-                    <h1 className='display-5 text-center'>
+                    <h1 className='display-5 text-center mb-5'>
                         An App to<br />
                         make your life<br />
                         <span className='display-2'>easy</span>
                     </h1>
-
-
+                    <img className='img-fluid' src={illustration} alt="" />
                 </div>
 
-                <div className='col-lg-6 h-100 d-flex align-items-center justify-content-center flex-column'></div>
+                <div className='col-lg-6 h-100 d-flex align-items-center justify-content-center flex-column'>
+                    <div className='card w-50'>
+                        <div className='card-header d-flex'>
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </div>
+                        <div className="card-body">
+                            <Outlet />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
